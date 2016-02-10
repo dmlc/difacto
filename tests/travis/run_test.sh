5c783b04f3dfb20b8304798105ed9787eb072a5e
Mu Li <muli@cs.cmu.edu>
2016-02-10 00:27:45 -0500

    Update run_test.sh

--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -6,6 +6,6 @@ if [ ${TASK} == "lint" ]; then
 fi
 
 if [ ${TASK} == "cpp-test" ]; then
-    make -j4 test CXX=g++-4.8 ADD_CFLAGS=-coverage
+    make -j4 test CXX=clang ADD_CFLAGS=-coverage
     cd build; find *_test -exec ./{} \;
 fi
